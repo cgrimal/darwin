f41f4a56d2070a6896761d941a270afc70109a2e
cgrimal <clement.grimal@gmail.com>
2017-08-09 21:19:05 +0200

    update site, pb link

--- darwin_create_database.py
+++ darwin_create_database.py
@@ -98,7 +98,7 @@ def extractData(data, d):
 
             if force or emission_hash not in hash_list:
 
-                emission_link = 'https://www.franceinter.fr' + pq(bb).find('a.rich-section-list-item-content-title').attr('href')
+                emission_link = pq(bb).find('a.rich-section-list-item-content-title').attr('href')
                 print emission_link
                 emission_data['lien_emission'] = emission_link
 
